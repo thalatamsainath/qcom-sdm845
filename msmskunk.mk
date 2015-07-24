69011c228f3a01ba9ca7d72c4661e8f62525257e
Ameya Thakur <ameyat@codeaurora.org>
2015-07-23 18:17:26 -0700

    Skunk target files
    Initial version of target files for skunk.This commit is written
    to get the build to compile and generate a version that will run a
    android console present on the ramdisk.
    - Do not expect anything fancy to work
    - Filesystem mounting has been disabled
    - Andorid frameworks,etc will not be started.
    - audio_effects.conf from aosp : commit:eb108a46228 Id: Id219d12
    - audio_policy.conf from aosp : commit: faca05e9 Id: Id219d121f81f687
    
    Change-Id: I5a3f8809b2c325b9ae20498f9141135117250091

---
+++ msmskunk.mk
@@ -0,0 +1,26 @@
+$(call inherit-product, device/qcom/common/common64.mk)
+
+PRODUCT_NAME := msmskunk
+PRODUCT_DEVICE := msmskunk
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := MSMSkunk for arm64
+
+TARGET_KERNEL_VERSION := 4.7
+# default is nosdcard, S/W button enabled in resource
+PRODUCT_CHARACTERISTICS := nosdcard
+
+#Android EGL implementation
+PRODUCT_PACKAGES += libGLES_android
+
+# Audio configuration file
+PRODUCT_COPY_FILES += \
+    device/qcom/msmskunk/audio_policy.conf:system/etc/audio_policy.conf \
+    device/qcom/msmskunk/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    device/qcom/msmskunk/mixer_paths.xml:system/etc/mixer_paths.xml \
+    device/qcom/msmskunk/mixer_paths_i2s.xml:system/etc/mixer_paths_i2s.xml \
+    device/qcom/msmskunk/audio_platform_info_i2s.xml:system/etc/audio_platform_info_i2s.xml
+
+# Listen configuration file
+PRODUCT_COPY_FILES += \
+    device/qcom/msmskunk/listen_platform_info.xml:system/etc/listen_platform_info.xml
+
