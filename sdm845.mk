e0ab052a8261cef41c9ef45d9c54456f7cd21410
Adam Bickett <abickett@codeaurora.org>
2017-07-23 21:57:27 -0700

    Use build variables to build device matrices.
    Move away from building device manifest through PRODUCT_COPY_FILES.
    Use the build variables - DEVICE_MANIFEST_FILE and DEVICE_MATRIX_FILE
    to build device manifest and device compatibility files repectively.
    
    Change-Id: Ibe1cfaeb09f3e0369acc6bf18ea10f737f448946

--- sdm845.mk
+++ sdm845.mk
@@ -106,9 +106,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml \
 
-# Adding vendor manifest
-PRODUCT_COPY_FILES += \
-    device/qcom/sdm845/vintf.xml:$(TARGET_COPY_OUT_VENDOR)/manifest.xml
+DEVICE_MANIFEST_FILE := device/qcom/sdm845/manifest.xml
+DEVICE_MATRIX_FILE   := device/qcom/common/compatibility_matrix.xml
 
 #ANT+ stack
 PRODUCT_PACKAGES += \
