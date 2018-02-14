f9c8ef732f99e6edf81d21a81b250c57092e8936
Garik Badalyan <garikb@codeaurora.org>
2018-02-14 10:12:43 -0800

    Move AtFwd hal from device to framework manifest.
    AtFwd hal is implemented as an android service and
    it stars from system partition, that is, it's not
    a traditional vendor hal and hence it should be
    declared in the framework manifest and not in the
    device manifest.
    
    This commit introduces new device specific framework
    manifest (framework_manifest.xml) and moves AtFwd hal
    from device to framework manifest.
    
    Change-Id: I65394f556fa796a3b0fc1058ff89018b269663ea
    CRs-Fixed: 2190055

--- sdm845.mk
+++ sdm845.mk
@@ -115,6 +115,7 @@ PRODUCT_PACKAGES_DEBUG += bootctl
 
 DEVICE_MANIFEST_FILE := device/qcom/sdm845/manifest.xml
 DEVICE_MATRIX_FILE   := device/qcom/common/compatibility_matrix.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE := device/qcom/sdm845/framework_manifest.xml
 
 #ANT+ stack
 PRODUCT_PACKAGES += \
